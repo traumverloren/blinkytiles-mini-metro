7a2c94ab2fe5cabbfbb9e74d76068ef5d530c00f
Stephanie <traumverloren@gmail.com>
2019-06-30 16:15:48 +0200

    fading to rainbow works better now!

--- blinkytiles-teensy.ino
+++ blinkytiles-teensy.ino
@@ -4,7 +4,6 @@
 #include <DmxSimple.h>
 #include <FastLED.h>
 #include <SoftwareSerial.h>
-#include <ArduinoJson.h>
 
 #if FASTLED_VERSION < 3001000
 #error "Requires FastLED 3.1 or later; check github for latest code."
@@ -16,7 +15,7 @@ SoftwareSerial HWSERIAL(6,7); // RX, TX
 #define NUM_LEDS    7
 #define LED_TYPE    DMXSIMPLE
 #define COLOR_ORDER BGR
-#define BRIGHTNESS   128
+#define BRIGHTNESS   200
 
 CRGB leds[NUM_LEDS];
 
@@ -31,13 +30,13 @@ mode currentMode = modeTwinkle;
 
 uint8_t framesPerSecond = 60;
 uint8_t newHue;
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+uint8_t gHue = 160; // rotating "base color" used by many of the patterns
 
 // Base background color
-CHSV BASE_COLOR(gHue, 255, 64);
+CHSV BASE_COLOR(gHue, 255, 60);
 
 // Peak color to twinkle up to
-CHSV PEAK_COLOR(gHue, 255, 192);
+CHSV PEAK_COLOR(gHue, 255, 180);
 
 // Currently set to brighten up a bit faster than it dims down, 
 // but this can be adjusted.
@@ -46,12 +45,12 @@ CHSV PEAK_COLOR(gHue, 255, 192);
 CHSV DELTA_COLOR_UP(gHue, 255, 12);
 
 // Amount to decrement the color by each loop as it gets dimmer:
-CHSV DELTA_COLOR_DOWN(gHue, 255, 12);
+CHSV DELTA_COLOR_DOWN(gHue, 255, 14);
 
 // Chance of each pixel starting to brighten up.  
 // 1 or 2 = a few brightening pixels at a time.
 // 10 = lots of pixels brightening at a time.
-uint8_t chanceOfTwinkle = 0;
+uint8_t chanceOfTwinkle = 1;
 
 // For receiving serial port messages from RPI
 byte peopleCount;
@@ -90,11 +89,6 @@ void loop() {
           break;
       default:
         break;
-    }
-
-    if (currentMode == modeTwinkle) {
-       // fade all existing pixels toward bgColor by "5" (out of 255)
-       fadeTowardColor( leds, NUM_LEDS, BASE_COLOR, 5);
     }
 
     // send the 'leds' array out to the actual LED strip
@@ -136,15 +130,30 @@ void showNewData() {
     if (newData == true) {
         Serial.print(peopleCount, HEX);
         Serial.println(' ');
-        if (peopleCount > 1) {
+        if (peopleCount > 0) {
           chanceOfTwinkle = 1;
-          currentMode = modeRainbow;
-        } else if (peopleCount == 1) {
-          chanceOfTwinkle = 1;
-          currentMode = modeFade;
+          if (currentMode != modeRainbow) {
+              for ( uint16_t i = 0; i < 255; i++) {
+                fadeTowardRainbow( leds, NUM_LEDS, 2);
+                  Serial.println(gHue);
+                  FastLED.show();
+                  FastLED.delay(10);
+              }
+              currentMode = modeRainbow;
+          }
+//        } else if (peopleCount == 1) {
+//          chanceOfTwinkle = 1;
+//          currentMode = modeTwinkle;
         } else if (peopleCount == 0) {
           chanceOfTwinkle = 0;
-          currentMode = modeFade;
+          if (currentMode != modeTwinkle) {
+              for ( uint16_t i = 0; i < 255; i++) {
+                  fadeTowardColor( leds, NUM_LEDS, BASE_COLOR, 2);
+                  FastLED.show();
+                  FastLED.delay(10);
+              }
+              currentMode = modeTwinkle;
+          }
         }
         newData = false;
     }
@@ -180,7 +189,6 @@ void addGlitter( fract8 chanceOfGlitter)
 }
 void confetti() 
 {
-  
   // random colored speckles that blink in and fade smoothly
   fadeToBlackBy(leds, NUM_LEDS, BRIGHTNESS*0.8);
   int pos = random16(NUM_LEDS);
@@ -262,5 +270,15 @@ void fadeTowardColor( CRGB* L, uint16_t N, const CRGB& bgColor, uint8_t fadeAmou
 {
   for( uint16_t i = 0; i < N; i++) {
     fadeTowardColor( L[i], bgColor, fadeAmount);
+  }
+}
+
+void fadeTowardRainbow( CRGB* L, uint16_t N, uint8_t fadeAmount)
+{
+  for( uint16_t i = 0; i < N; i++) {
+    CHSV hsv( gHue+(i*7), 255, 255);
+    CRGB rgb;
+    hsv2rgb_rainbow( hsv, rgb);
+    fadeTowardColor( L[i], rgb, fadeAmount);
   }
 }
